7ac9ce7264893d5efc9b8f5a38901e49b207a56d
qmo130240 <qmo1320240@gmail.com>
2020-05-26 12:39:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,14 +11,20 @@ async def on_ready(): #bot start
 @bot.event
 async def on_member_join(member): #someone join the discord server
     #print(F'{member} join!')
-    channel = bot.get_channel(714691114313056256) #bot will get channel's ID
+    channel = bot.get_channel(528522034788171778) #bot will get channel's ID
     await channel.send(F'{member} join!')
 
 @bot.event
 async def on_member_remove(member):#someone leave the discord server
     #print(F'{member} leave!')
-    channel = bot.get_channel(714691155333349387) #bot will get channel's ID
+    channel = bot.get_channel(528522034788171778) #bot will get channel's ID
     await channel.send(F'{member} leave QQ')
 
-bot.run('NzE0NDI3ODU0ODIxMTMwMjYw.XsupjA.nPvK0q7yN5d0FeEhf2uH6pwZDs8')
+@bot.command()
+async def ping(ctx): #bot's delay
+    await ctx.send(F'{round(bot.latency*1000)}(ms)')
+    
 
+
+
+bot.run('NzE0NDI3ODU0ODIxMTMwMjYw.XsybtQ.r89mYE05qUfIEs3LkGvi8_WmBHY')
